eecbec3776a4174fe045a656f8a5341c90fa6583
yazid <yazid@example.com>
2025-08-05 02:05:50 +0100

    jest project

---
+++ __tests__/math.test.js
@@ -0,0 +1,9 @@
+const { add, subtract } = require('../src/math');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('subtracts 5 - 3 to equal 2', () => {
+    expect(subtract(5, 3)).toBe(2);
+});
