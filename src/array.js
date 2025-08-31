7540212c71d67273857d793f25f69f2aa1e2b97b
hamzaoyosf <hamzaoyosf@gmail.com>
2025-08-31 19:04:47 +0100

    update my exercises

--- src/array.js
+++ src/array.js
@@ -1,26 +1,21 @@
+//Question 1
 let arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41];
 let primeNumbers = primeNumber(arr)
-
 function primeNumber(){
-    
-
     for (let i = 0; i < arr.length; i++)
-        if (arr[i] % 1 === 0){
+        if (arr[i] % 1 === 0 && arr [i] % arr [i] === 0){
             return true
-        } else {return false}
+        } else return false
 }
 
-
+//Question 2
 let average = averageOfArr(arr)
-
 function averageOfArr(){
-    
-    let sum = 0;
+    let avrg = 0;
     for (let i = 0; i < arr.length; i++){
-        sum += arr[i];
+        avrg += arr[i] / arr.length;
     }
-    return sum
+    return avrg
 }
-
 
 module.exports={primeNumbers, average};
